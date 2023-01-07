6a3d37ab71bb29a70cc082b60cb611cfb64ef8ed
Tommie Nygren <tommie.nygren@gmail.com>
2023-01-07 14:54:25 +0100

    Created factories

--- core/src/main/kotlin/depth/core/Screen3d.kt
+++ core/src/main/kotlin/depth/core/Screen3d.kt
@@ -10,7 +10,7 @@ import ktx.app.clearScreen
 open class Screen3d(protected val game: MainGame,
                     protected val engine: Engine, protected val viewport: ExtendViewport
 ) : KtxScreen {
-    private val bgColor = "AAAA75".toColor()
+    private val bgColor = "2244DD".toColor()
     override fun render(delta: Float) {
         clearScreen(bgColor.r, bgColor.g, bgColor.b)
         engine.update(delta)
---
+++ core/src/main/kotlin/depth/injection/Factories.kt
@@ -0,0 +1,5 @@
+package depth.injection
+
+fun createSubMarine() {
+    
+}
