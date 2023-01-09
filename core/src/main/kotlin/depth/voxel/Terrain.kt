015494a01f147d12c0236b8a6db2a5e837201730
Tommie Nygren <tommie.nygren@gmail.com>
2023-01-09 19:36:12 +0100

    ARgh

--- core/src/main/kotlin/depth/voxel/Terrain.kt
+++ core/src/main/kotlin/depth/voxel/Terrain.kt
@@ -7,14 +7,14 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
 import com.badlogic.gdx.utils.Disposable
 import com.github.tommyettinger.digital.MathTools.norm
-import com.sudoplay.joise.mapping.Mapping
-import com.sudoplay.joise.mapping.MappingMode
+import com.sudoplay.joise.mapping.*
 import com.sudoplay.joise.module.ModuleAutoCorrect
 import com.sudoplay.joise.module.ModuleBasisFunction
 import com.sudoplay.joise.module.ModuleBasisFunction.BasisType
 import com.sudoplay.joise.module.ModuleScaleDomain
 import ktx.log.info
 import make.some.noise.Noise
+import kotlin.math.min
 
 
 abstract class Terrain(
@@ -29,13 +29,15 @@ open class HeightMapTerrain(points: Int, size: Float, heightMagnitude: Float) : 
     private val noise = Noise((0..1000).random(), 1f / 4f, Noise.CELLULAR)
     private val heightValues = Array(points) { x ->
         Array(points) { y ->
-            0f
+            0.0f
         }
-    }.flatten().toFloatArray()
+    }
     init {
+        var minVal = 0f
+        var maxval = 0f
         val basis = ModuleBasisFunction()
         basis.setType(BasisType.SIMPLEX)
-        basis.seed = 42
+        basis.seed = (1..1000).random().toLong()
 
         val correct = ModuleAutoCorrect()
         correct.setSource(basis)
@@ -43,10 +45,24 @@ open class HeightMapTerrain(points: Int, size: Float, heightMagnitude: Float) : 
 
         val scaleDomain = ModuleScaleDomain()
         scaleDomain.setSource(correct)
-        scaleDomain.setScaleX(4.0)
-        scaleDomain.setScaleY(4.0)
-        scaleDomain
-        Mapping.map3D(MappingMode.SEAMLESS_XYZ, points, points, points,scaleDomain)
+        val scale = 64.0
+        scaleDomain.setScaleX(scale)
+        scaleDomain.setScaleY(scale)
+        scaleDomain.setScaleZ(scale)
+        scaleDomain.setScaleU(scale)
+        scaleDomain.setScaleW(scale)
+        Mapping.map2DNoZ(MappingMode.SEAMLESS_XY, points, points, scaleDomain, MappingRange.DEFAULT,
+            IMapping2DWriter { x, y, value ->
+                if(value < minVal) {
+                    minVal = value.toFloat()
+                    info { "Min: $minVal" }
+                }
+                if(value > maxVal) {
+                    maxVal = value.toFloat()
+                    info { "Max: $maxVal" }
+                }
+                heightValues[x][y] = value.toFloat()
+                             }, IMappingUpdateListener.NULL_LISTENER)
 
     }
     private val otherNoise by lazy {
@@ -71,7 +87,7 @@ open class HeightMapTerrain(points: Int, size: Float, heightMagnitude: Float) : 
 
     private val heightField = HeightField(
         true,
-        heightValues,
+        heightValues.flatten().toFloatArray(),
         points,
         points,
         true,
