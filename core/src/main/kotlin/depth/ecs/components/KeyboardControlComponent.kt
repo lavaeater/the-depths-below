d4763376221a7c3288b8c880ac0a7ccac92100ca
Tommie Nygren <tommie.nygren@gmail.com>
2023-01-13 10:07:57 +0100

    Yay, better controls!

--- core/src/main/kotlin/depth/ecs/components/KeyboardControlComponent.kt
+++ core/src/main/kotlin/depth/ecs/components/KeyboardControlComponent.kt
@@ -14,11 +14,41 @@ sealed class Direction {
     object Reverse: Direction()
     object Neutral: Direction()
 }
+sealed class Rotation {
+    object YawLeft: Rotation()
+    object YawRight: Rotation()
+    object PitchUp: Rotation()
+    object PitchDown: Rotation()
+    object RollLeft: Rotation()
+    object RollRight: Rotation()
+}
 
 class KeyboardControlComponent: Component, Pool.Poolable {
-    var throttle: Direction = Direction.Neutral
-    var horizontal: Direction = Direction.Neutral
-    var vertical: Direction = Direction.Neutral
+    val orthogonal = mutableSetOf<Direction>()
+    val rotational = mutableSetOf<Rotation>()
+
+    fun has(direction: Direction) : Boolean {
+        return orthogonal.contains(direction)
+    }
+
+    fun has(rotation: Rotation): Boolean {
+        return rotational.contains(rotation)
+    }
+
+    fun add(direction: Direction) {
+        orthogonal.add(direction)
+    }
+    fun remove(direction: Direction) {
+        orthogonal.remove(direction)
+    }
+
+    fun add(rotation: Rotation) {
+        rotational.add(rotation)
+    }
+    fun remove(rotation: Rotation) {
+        rotational.remove(rotation)
+    }
+
     override fun reset() {
 
     }
--- core/src/main/kotlin/depth/ecs/systems/SubmarineControlSystem.kt
+++ core/src/main/kotlin/depth/ecs/systems/SubmarineControlSystem.kt
@@ -3,12 +3,8 @@ package depth.ecs.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.systems.IteratingSystem
 import com.badlogic.gdx.Input.Keys
-import com.badlogic.gdx.math.Quaternion
 import com.badlogic.gdx.math.Vector3
-import depth.ecs.components.Direction
-import depth.ecs.components.KeyboardControlComponent
-import depth.ecs.components.SceneComponent
-import depth.ecs.components.Transform3d
+import depth.ecs.components.*
 import eater.input.KeyPress
 import eater.input.command
 import ktx.app.KtxInputAdapter
@@ -22,7 +18,9 @@ class SubmarineControlSystem :
     IteratingSystem(
         allOf(
             KeyboardControlComponent::class,
-            SceneComponent::class).get()),
+            SceneComponent::class
+        ).get()
+    ),
     KtxInputAdapter {
     private val family = allOf(KeyboardControlComponent::class).get()
 
@@ -35,40 +33,38 @@ class SubmarineControlSystem :
         setBoth(
             Keys.W,
             "Throttle F",
-            { controlComponent.throttle = Direction.Neutral },
-            {
-                controlComponent.throttle = Direction.Forward
-            }
+            { controlComponent.remove(Direction.Forward) },
+            { controlComponent.add(Direction.Forward) }
         )
         setBoth(
             Keys.S,
             "Throttle R",
-            { controlComponent.throttle = Direction.Neutral },
-            { controlComponent.throttle = Direction.Reverse }
+            { controlComponent.remove(Direction.Neutral) },
+            { controlComponent.add(Direction.Reverse) }
         )
         setBoth(
             Keys.A,
             "Left",
-            { controlComponent.horizontal = Direction.Neutral },
-            { controlComponent.horizontal = Direction.Left }
+            { controlComponent.remove(Rotation.YawLeft) },
+            { controlComponent.add(Rotation.YawLeft) }
         )
         setBoth(
             Keys.D,
             "Right",
-            { controlComponent.horizontal = Direction.Neutral },
-            { controlComponent.horizontal = Direction.Right }
+            { controlComponent.remove(Rotation.YawRight) },
+            { controlComponent.add(Rotation.YawRight) }
         )
         setBoth(
             Keys.UP,
             "Up",
-            { controlComponent.vertical = Direction.Neutral },
-            { controlComponent.vertical = Direction.Up }
+            { controlComponent.remove(Direction.Up) },
+            { controlComponent.add(Direction.Up) }
         )
         setBoth(
             Keys.DOWN,
             "Down",
-            { controlComponent.vertical = Direction.Neutral },
-            { controlComponent.vertical = Direction.Down }
+            { controlComponent.remove(Direction.Down) },
+            { controlComponent.add(Direction.Down) }
         )
     }
 
@@ -80,38 +76,39 @@ class SubmarineControlSystem :
         return controlMap.execute(keycode, KeyPress.Up)
     }
 
+    val validDirections = listOf(Direction.Up, Direction.Down, Direction.Forward, Direction.Reverse)
+    val validRotations = listOf(Rotation.YawLeft, Rotation.YawRight)
+
     override fun processEntity(entity: Entity, deltaTime: Float) {
         val sc = SceneComponent.get(entity).scene
         val tf = Transform3d.get(entity)
         val position = tf.position
-        when(controlComponent.horizontal) {
-            Direction.Left -> {
-                tf.forward.rotate(Vector3.Y, speed * deltaTime)
-                sc.modelInstance.transform.rotate(Vector3.Y, speed * deltaTime)
-            }
-            Direction.Right -> {
-                tf.forward.rotate(Vector3.Y, -speed * deltaTime)
-                sc.modelInstance.transform.rotate(Vector3.Y, -speed * deltaTime)
-            }
-            else -> {}
+
+        if (controlComponent.has(Rotation.YawLeft)) {
+            tf.forward.rotate(Vector3.Y, speed * deltaTime)
+            sc.modelInstance.transform.rotate(Vector3.Y, speed * deltaTime)
         }
-        when(controlComponent.vertical) {
-            Direction.Up -> {
-                sc.modelInstance.transform.setTranslation(position + vec3(0f,speed*deltaTime,0f) )
-            }
-            Direction.Down -> {
-                sc.modelInstance.transform.setTranslation(position + vec3(0f,-speed*deltaTime,0f) )
-            }
-            else -> {}
+
+        if (controlComponent.has(Rotation.YawRight)) {
+            tf.forward.rotate(Vector3.Y, -speed * deltaTime)
+            sc.modelInstance.transform.rotate(Vector3.Y, -speed * deltaTime)
         }
-        when(controlComponent.throttle) {
-            Direction.Forward -> {
-                sc.modelInstance.transform.setTranslation(position + tf.forward * (speed * deltaTime))
-            }
-            Direction.Reverse -> {
-                sc.modelInstance.transform.setTranslation(position + tf.forward * (-speed * deltaTime))
-            }
-            else -> {}
+
+        val targetPosition = position.cpy()
+        if (controlComponent.has(Direction.Up)) {
+            targetPosition.add(0f, speed * deltaTime, 0f)
         }
+        if (controlComponent.has(Direction.Down)) {
+            targetPosition.add(0f, -speed * deltaTime, 0f)
+        }
+        if (controlComponent.has(Direction.Forward)) {
+            targetPosition.add(tf.forward * (speed * deltaTime))
+        }
+        if (controlComponent.has(Direction.Reverse)) {
+            targetPosition.add(tf.forward * (-speed * deltaTime))
+        }
+
+
+        sc.modelInstance.transform.setTranslation(targetPosition)
     }
 }
