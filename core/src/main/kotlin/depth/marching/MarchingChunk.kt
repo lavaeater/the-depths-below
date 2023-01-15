4f042811966eb58ee87459b7b1f1da8e7c271099
Tommie Nygren <tommie.nygren@gmail.com>
2023-01-15 14:53:36 +0100

    Some minor refactorings

---
+++ core/src/main/kotlin/depth/marching/MarchingChunk.kt
@@ -0,0 +1,18 @@
+package depth.marching
+
+import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.physics.bullet.Bullet
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody
+import depth.ecs.components.MotionState
+import net.mgsx.gltf.scene3d.scene.Scene
+
+class MarchingChunk(val modelInstance: ModelInstance) {
+    val scene = Scene(modelInstance)
+    val collisionShape = Bullet.obtainStaticNodeShape(modelInstance.model.nodes)
+    val motionState = MotionState().apply {
+        transform = modelInstance.transform
+    }
+    val rigidBody = btRigidBody(btRigidBody.btRigidBodyConstructionInfo(0f, motionState, collisionShape, Vector3.Zero))
+
+}
---
+++ core/src/main/kotlin/depth/marching/WorldManager.kt
@@ -0,0 +1,7 @@
+package depth.marching
+
+import com.badlogic.gdx.physics.bullet.dynamics.btDynamicsWorld
+
+class WorldManager(
+    private val world: btDynamicsWorld
+)
